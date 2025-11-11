24529ae314c558b60f5f39df559092dc99f2f1c4
Patrickson2 <thairupatrickson@gmail.com>
2025-11-12 00:05:35 +0300

    generated an endless loop using while

---
+++ loop.py
@@ -0,0 +1,22 @@
+# while =>executes a statement number of times while the condition is true 
+    # EXAMPLE CODE 1
+
+count = 0
+
+while count <= 10:
+    print("Kiongozi")
+    count += 1  
+
+# for >= used to iterate over the sequence
+    # EXAMPLE CODE 2
+
+
+Fruit = ["Apple", "Orange","Kiwi","Mango"]
+
+for Fruit in Fruit:
+    if Fruit =="Kiwi":
+        print("Healthy")
+        break 
+
+
+

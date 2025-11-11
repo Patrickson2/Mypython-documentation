e6b3d64dd0c87143be57800aebf12f05935c2e83
Patrickson2 <thairupatrickson@gmail.com>
2025-11-11 15:26:33 +0300

    created conditional statements

---
+++ condition.py
@@ -0,0 +1,23 @@
+# Conditional statements 
+# if , else, elif
+
+age = 20 
+
+if age > 18:
+    print("You are an adult")
+else:
+    print("You are underAge") 
+
+marks = 40 
+if marks >= 90:
+    print('A')
+elif marks >= 80: 
+    print ('B')    
+elif marks >= 70: 
+    print ('C')
+elif marks >= 60: 
+    print ('D') 
+elif marks >= 50: 
+    print ('E')    
+else:
+    print('F')                   

1611d70476fef7c1b0c5eca2ea26fa0e4c59e83d
Patrickson2 <thairupatrickson@gmail.com>
2025-11-12 13:03:09 +0300

    created a function and tride to overide it

---
+++ func.py
@@ -0,0 +1,16 @@
+# my function display
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+# greet_programmer = "Hello, programmer!"
+greet_programmer()
+
+# on this code i cannot excute because i am overiding the entire code wile still calling line 6
+            #  python3 func.py
+            #  Traceback (most recent call last):
+            #    File "/home/mungai/Documents/moringaProjects/phase3/python-class/func.py", line 7, in <module>
+                #  greet_programmer()
+            #  TypeError: 'str' object is not callable
+
+# so to go about this i commented on line 6 to remove the overide of the code.

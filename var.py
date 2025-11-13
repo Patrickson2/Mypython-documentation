72eae0946fc9f29f0b2da1bdd2652f05904bc803
Patrickson2 <thairupatrickson@gmail.com>
2025-11-13 14:25:24 +0300

    showcased how both the local and global work

--- var.py
+++ var.py
@@ -27,3 +27,29 @@ def change_name():
 greet()
 change_name()
 print("Outside:", name)
+
+
+# when wanting To change the value of a global variable from a local scope, we need to use the global keyword eg:
+change_the_world = "change yourself"
+
+def change_yourself():
+    global change_the_world
+    change_the_world = "world changed"
+
+change_yourself() 
+print(change_the_world)
+# print(change_yourself)
+
+# on this code below shows why you cannot print change yourself again on line 41 because python will like ignore it and produce it as a memory address
+def say_hi():
+    print("Hi there!")
+
+print(say_hi)
+say_hi()
+
+# this is like a break down of the code on assigning the global keyword
+change_the_world = "change yourself"
+
+def change():
+    print(change_the_world)
+change()

3c30a6c4209bc6a1b75addd30ae299302d7a63b2
Patrickson2 <thairupatrickson@gmail.com>
2025-11-13 15:16:27 +0300

    learning on operators

---
+++ opp.py
@@ -0,0 +1,38 @@
+# arithmetic operators 
+a = 10
+b = 3
+# print(a + b)
+# print(a * b)
+# print(a / b)
+# print(a - b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+
+
+# comparison
+# ==, <, >, !=, >=, <=
+# NB return value here should be booleaan
+# print(a == b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a != b)
+
+# Logical operators
+# AND, OR, NOT
+
+# age = 16 
+# canVote = True
+
+# if age >= 18 and canVote:
+#     print("You are an Adult")
+# else:
+#     print("You are under age")
+
+
+# if age >= 18 or canVote:
+#     print("You are an Adult")
+# else:
+#     print("You are under age")

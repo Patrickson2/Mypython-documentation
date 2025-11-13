acd7ffebc70c317f0d5804e5d77608d9b2e94fb8
Patrickson2 <thairupatrickson@gmail.com>
2025-11-13 13:58:54 +0300

    diff btwn global & local

--- var.py
+++ var.py
@@ -1,3 +1,19 @@
+message = "Hello from outside"
+
+def greet():
+    print(message)
+    greet()
+    print(message) 
+
+
+def greet():
+    message = "Hello from inside the function"
+    print(message)  
+
+greet()
+# print(message)  
+# on the line above will bring an error message that the message is not defined thus making it a comment 
+
 name = "Patrickson"
 
 def greet():

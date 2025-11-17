839996d19b26bb5faca2976525de6a50d0d18db9
Patrickson2 <thairupatrickson@gmail.com>
2025-11-17 11:57:15 +0300

    did some quizes for rem

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+student_details = {
+    "first_name": "Patickson",
+    "second_name": "Mungai",
+    "address": {
+        "city": "Nairobi",
+        "zip_code": "00100"
+    },
+    "friends": ["Albert", "Gabriel", "Bonface", "Mary"]
+}
+
+print(student_details["first_name"])
+print(student_details["second_name"])
+print(student_details["address"]["city"])
+print(student_details["address"]["zip_code"])
+print(student_details["friends"][2])

eb883d9c2213b9e7d07846fc2efdbd8a2a664b8f
Patrickson2 <thairupatrickson@gmail.com>
2025-11-19 01:13:31 +0300

    example

---
+++ ex.py
@@ -0,0 +1,10 @@
+class Human:
+    def __init__(self, name, age="40"):
+        self.name = name
+        self.age = age
+h1 = Human("Kamau", 20)
+h2 = Human("kamara")
+print(h1.name, h1.age)   
+print(h2.name, h2.age)     
+
+
